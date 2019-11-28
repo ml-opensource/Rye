d6a0d9fa046f79b80a864a5eecc93e7f8daa3655
Belle Beth Cooper <bellebethcooper@gmail.com>
2019-11-28 18:08:14 +1100

    Changed unchanged variable to a constant

--- Rye/Rye/RyeViewController.swift
+++ Rye/Rye/RyeViewController.swift
@@ -39,7 +39,7 @@ public class RyeViewController: UIViewController {
                 return topController.view
 
             } else if #available(iOS 13.0, *),
-                var windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
                     var topController = windowScene.windows[0].rootViewController {
                     while let presentedViewController = topController.presentedViewController {
                         topController = presentedViewController
