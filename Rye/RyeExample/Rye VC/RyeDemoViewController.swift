ad58bf872fd493b5ae4b453794723d0b8b35c506
Peter Bødskov <pebo@nodes.dk>
2020-03-06 15:36:30 +0100

    Improved demo app with new features
    fixes #31

--- Rye/RyeExample/Rye VC/RyeDemoViewController.swift
+++ Rye/RyeExample/Rye VC/RyeDemoViewController.swift
@@ -9,93 +9,139 @@
 import UIKit
 import Rye
 
-class RyeDemoViewController: UIViewController {
+class RyeDemoViewController: UITableViewController {
     
+    // MARK: - Outlets
+    @IBOutlet weak var ryeMessageTextField: UITextField!
+    @IBOutlet weak var dismissRyeButton: UIButton!
+    
+    // MARK: - Variables
     var ryeViewController: RyeViewController?
     var isShowingNonDismissableMessage: Bool = false
-
+    
+    let contentInset: CGFloat = 20.0
+    var dismissMode: Rye.DismissMode = .automatic(interval: Rye.defaultDismissInterval)
+    var viewType: Rye.ViewType = .standard(configuration: nil)
+    var position: Rye.Position = .top(inset: 20.0)
+    var animationType: Rye.AnimationType = .slideInOut
+    let customView = RyeImageView.fromNib()
+    
+    // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-        
-    private func displayAutomaticMessage() {
-        let ryeConfiguration: RyeConfiguration = [
-            .text : "This message will self destruct after \(Rye.defaultDismissInterval) seconds",
-            .backgroundColor: UIColor.red.withAlphaComponent(0.7),
-            .animationType: Rye.AnimationType.slideInOut
-        ]
-        
-        ryeViewController = RyeViewController(dismissMode: .automatic(interval: Rye.defaultDismissInterval),
-                                    viewType: .standard(configuration: ryeConfiguration),
-                                    at: .top(inset: 40.0))
-        ryeViewController?.show(withDismissCompletion: {
-            self.ryeViewController = nil
-        })
+        tableView.tableFooterView = UIView()
+        dismissRyeButton.isHidden = true
     }
     
-
-    private func displayGestureMessage() {
-        let ryeConfiguration: RyeConfiguration = [
-            .text : "Tap or swipe this message to dismiss it",
-            .backgroundColor: UIColor.lightGray.withAlphaComponent(0.7),
-            .animationType: Rye.AnimationType.slideInOut
-        ]
-        
-        ryeViewController = RyeViewController(dismissMode: .gesture,
-                                    viewType: .standard(configuration: ryeConfiguration),
-                                    at: .top(inset: 0.0))
-        ryeViewController?.show(withDismissCompletion: {
-            self.ryeViewController = nil
-        })
-    }
-
-    private func displayNonDismissableMessage() {
-        let ryeConfiguration: RyeConfiguration = [
-            .text : "This message will stay till you tap the button again",
-            .backgroundColor: UIColor.blue.withAlphaComponent(0.7),
-            .animationType: Rye.AnimationType.fadeInOut
-        ]
-        
-        ryeViewController = RyeViewController(dismissMode: .nonDismissable,
-                                              viewType: .standard(configuration: ryeConfiguration),
-                                    at: .top(inset: 0.0))
-        ryeViewController?.show(withDismissCompletion: {
-            self.ryeViewController = nil
-        })
-    }
-
-    private func displayCustomRyeMessage() {
-        let customRyeView = RyeButtonView.fromNib()
-        customRyeView.delegate = self
-//        let customRyeView = RyeImageView.fromNib()
-        
-        ryeViewController = RyeViewController(viewType: .custom(customRyeView, animationType: .fadeInOut),
-                                    at: .bottom(inset: 0.0))
-        ryeViewController?.show()
-    }
-    
-    @IBAction func didTapAutomatic(_ sender: UIButton) {
-        displayAutomaticMessage()
-    }
-    
-    @IBAction func didTapGesture(_ sender: UIButton) {
-        displayGestureMessage()
-    }
-
-    @IBAction func didTapNonDissmisable(_ sender: UIButton) {
-        if isShowingNonDismissableMessage {
-            ryeViewController?.dismiss()
-        } else {
-            displayNonDismissableMessage()
+    // MARK: - TableView methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0, 1:
+            break
+        case 2:
+            updateDismissMode(for: indexPath)
+        case 3:
+            updateViewMode(for: indexPath)
+        case 4:
+            updatePosition(for: indexPath)
+        case 5:
+            updateAnimationType(for: indexPath)
+        default:
+            break
         }
-        isShowingNonDismissableMessage.toggle()
     }
     
-}
+    private func updateDismissMode(for indexPath: IndexPath) {
+        for row in 0...2 {
+            tableView.cellForRow(at: IndexPath(row: row, section: indexPath.section))?.accessoryType = .none
+        }
+        tableView.cellForRow(at: IndexPath(row: indexPath.row, section: indexPath.section))?.accessoryType = .checkmark
+        switch indexPath.row {
+        case 0:
+            dismissMode = .automatic(interval: Rye.defaultDismissInterval)
+        case 1:
+            dismissMode = .gesture
+        case 2:
+            dismissMode = .nonDismissable
+        default:
+            break
+        }
+    }
+    
+    private func updateViewMode(for indexPath: IndexPath) {
+        for row in 0...1 {
+            tableView.cellForRow(at: IndexPath(row: row, section: indexPath.section))?.accessoryType = .none
+        }
+        tableView.cellForRow(at: IndexPath(row: indexPath.row, section: indexPath.section))?.accessoryType = .checkmark
+        switch indexPath.row {
+        case 0:
+            viewType = .standard(configuration: nil)
+        case 1:
+            viewType = .custom(customView, animationType: animationType)
+        default:
+            break
+        }
+    }
 
-extension RyeDemoViewController: RyeButtonViewDelegate {
-    func didTapButton(in sender: RyeButtonView) {
+    private func updatePosition(for indexPath: IndexPath) {
+        for row in 0...1 {
+            tableView.cellForRow(at: IndexPath(row: row, section: indexPath.section))?.accessoryType = .none
+        }
+        tableView.cellForRow(at: IndexPath(row: indexPath.row, section: indexPath.section))?.accessoryType = .checkmark
+        switch indexPath.row {
+        case 0:
+            position = .top(inset: contentInset)
+        case 1:
+            position = .bottom(inset: contentInset)
+        default:
+            break
+        }
+    }
+
+
+    private func updateAnimationType(for indexPath: IndexPath) {
+        for row in 0...1 {
+            tableView.cellForRow(at: IndexPath(row: row, section: indexPath.section))?.accessoryType = .none
+        }
+        tableView.cellForRow(at: IndexPath(row: indexPath.row, section: indexPath.section))?.accessoryType = .checkmark
+        switch indexPath.row {
+        case 0:
+            animationType = .slideInOut
+        case 1:
+            animationType = .fadeInOut
+        default:
+            break
+        }
+    }
+    
+    // MARK: - IBActions
+    @IBAction func didTapGenerateMessage(_ sender: UIButton) {
+        var selectedViewType = viewType
+        
+        if case Rye.ViewType.standard = viewType {
+            let ryeConfiguration: RyeConfiguration = [
+                .text : ryeMessageTextField.text ?? "",
+                .backgroundColor: UIColor.black.withAlphaComponent(0.5),
+                .animationType: animationType
+            ]
+            selectedViewType = .standard(configuration: ryeConfiguration)
+        }
+        
+        ryeViewController = RyeViewController(dismissMode: dismissMode,
+                                              viewType: selectedViewType,
+                                              at: position)
+        
+        ryeViewController?.show(withDismissCompletion: {
+            self.ryeViewController = nil
+        })
+        
+        if case Rye.DismissMode.nonDismissable = dismissMode {
+            dismissRyeButton.isHidden = false
+        }
+    }
+    
+    @IBAction func didTapDismissMessage(_ sender: UIButton) {
         ryeViewController?.dismiss()
+        dismissRyeButton.isHidden = true
     }
 }
-
